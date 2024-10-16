a20d5f9f017b9356a9e03e9290e959ba2f4558ff
Iam-Rajkumar <153513241+RajkumarReddy021@users.noreply.github.com>
2024-10-17 00:01:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "Created_by_terra" {
   ami           = "ami-04e81358dc84d661d"  # Replace with a valid AMI ID
   instance_type = "t2.micro"
 
